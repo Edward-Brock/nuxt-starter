5e7a359e2467bdc5d6525af887f8ef2cf9eb38f8
Edward Brock <1416477069@qq.com>
2025-03-31 20:07:44 +0800

    chore: 添加 @nuxt/eslint  依赖

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
   future: {
-    compatibilityVersion: 4
-  }
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-11-01',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
